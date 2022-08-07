b03a247fe17c89375f8dcad86f0b9e61d1b62b14
Alausa Aishat <qtaisha.ao@gmail.com>
2022-08-07 07:16:57 +0100

    working on lab

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -1,8 +1,17 @@
 import React from "react";
 import { directors } from "../data";
+import {nanoid} from "nanoid";
 
 function Directors() {
-  return <div>{/*{code here}*/}</div>;
+  return <div>{/*{code here}*/}
+  <h1>Directors Page</h1>
+  {directors.map(director =>
+    <div key = {nanoid(5)}>
+      {director.name}
+      <ul>{director.movies.map(movie =>
+        <li key = {nanoid(5)}>{movie}</li>)}</ul>
+    </div>)}
+  </div>;
 }
 
 export default Directors;
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,8 +1,21 @@
 import React from "react";
 import { movies } from "../data";
+import {nanoid} from "nanoid"
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  return <div>{/*{code here}*/}
+  <h1>Movies Page</h1>
+  {movies.map(movie =>
+    <div key = {nanoid(5)}>
+      {movie.title}
+      {movie.time}
+      <ul>{movie.genres.map(genre =>
+      <li key = {nanoid(5)}>{genre}</li>
+      )}
+      </ul>
+    </div>
+    )}
+  </div>
 }
 
 export default Movies;
